530cf58ff260c9f3132ac92736b5235efabe0cdf
Kelly Hondula <khondula@sesync.org>
2016-11-30 16:04:35 -0500

    updated patches slide

--- worksheet-1.R
+++ worksheet-1.R
@@ -129,3 +129,38 @@ library(ggplot2)
 
 ggplot(df, aes(x = n_wolves, y = time)) + 
   geom_boxplot()
+
+##################################
+#   Data frame to Netlogo List   #
+##################################
+
+# create a data.frame with four sample vectors
+list1 <- c(1,2,3,4,5)
+list2 <- c(6,7,8,9,10)
+list3 <- c('test1','test2','test3','test4','test5')
+list4 <- c(TRUE,FALSE,TRUE,FALSE,TRUE)
+
+sample.df <- data.frame(list1,list2,list3,list4)
+# 
+# # fill automaticlly the four NetLogo list with the names "list1", "list2", "list3" and "list4" (defined in "globals")
+# # with the values of the corresponding columns of the data.frame 
+NLDfToList(sample.df)
+# 
+# # show content of the NetLogo list in NetLogo Command Center
+# NLCommand("show-lists")
+
+#############
+#   Agents  #
+#############
+
+# use NLGetAgentSet()
+# arguments are agent.var, agentset
+# optional arguments are as.data.frame, agents.by.row, as.vector
+
+#############
+#  Patches  #
+#############
+
+# use NLGetPatches()
+# use NLSetPatches()
+
