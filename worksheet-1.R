dcb732bc236c4cffab1f4eb370a63d287c1e15f3
Kelly Hondula <khondula@sesync.org>
2016-11-30 16:55:34 -0500

    updated worksheet and show results for agent slide

--- worksheet-1.R
+++ worksheet-1.R
@@ -36,30 +36,33 @@ NLCommand(...)
 
 # Repeat commands for multiple iterations
 
-NLDoCommand(iterations = 50, "go")
+...(... =  50, "go")
 
 # Use an R object in a command
 
 init_sheep <- 50
-NLCommand("set initial-number-sheep", init_sheep, "setup")
+
+NLCommand("set ...", ... , "setup")
 
 # Reporting
 
-NLReport("count sheep")
+NLReport(...)
 
 # Combine commands and reporting
 
-NLDoReport(iterations = 5, command = "go", reporter = "count sheep")
+...(iterations = ... , 
+    ... = "go" , 
+    ... = "count sheep")
 
 # Return results as a data frame
 
-NLCommand("setup")
+NLCommand(... = )
 
-sheep_count <- NLDoReport(iterations = 100,
+sheep_count <- .__C__.environment(iterations = 100,
                           command = "go",
-                          reporter = c("ticks", "count sheep", "count wolves"),
-                          as.data.frame = TRUE,
-                          df.col.names = c("tick", "sheep", "wolves"))
+                          reporter = ... ,
+                          ... = TRUE,
+                          ... = c("tick", "sheep", "wolves"))
 
 tail(sheep_count)
 
@@ -67,9 +70,9 @@ tail(sheep_count)
 #  Use Conditionals     #
 #########################
 
-NLCommand("setup")
+NLCommand(...) 
 
-sim_results <- NLDoReportWhile(condition = "any? wolves",
+sim_results <- ... (... ,
                                command = "go", 
                                reporter = c("ticks", "count sheep", "count wolves"),
                                as.data.frame = TRUE,
@@ -120,8 +123,6 @@ df <- data.frame(as.factor(nw), unlist(sim_results))
 
 names(df) <- c("n_wolves", "time")
 
-head(df)
-
 # plot using ggplot
 
 install.packages("ggplot2")
@@ -130,37 +131,23 @@ library(ggplot2)
 ggplot(df, aes(x = n_wolves, y = time)) + 
   geom_boxplot()
 
-##################################
-#   Data frame to Netlogo List   #
-##################################
-
-# create a data.frame with four sample vectors
-list1 <- c(1,2,3,4,5)
-list2 <- c(6,7,8,9,10)
-list3 <- c('test1','test2','test3','test4','test5')
-list4 <- c(TRUE,FALSE,TRUE,FALSE,TRUE)
-
-sample.df <- data.frame(list1,list2,list3,list4)
-# 
-# # fill automaticlly the four NetLogo list with the names "list1", "list2", "list3" and "list4" (defined in "globals")
-# # with the values of the corresponding columns of the data.frame 
-NLDfToList(sample.df)
-# 
-# # show content of the NetLogo list in NetLogo Command Center
-# NLCommand("show-lists")
 
 #############
 #   Agents  #
 #############
 
-# use NLGetAgentSet()
-# arguments are agent.var, agentset
-# optional arguments are as.data.frame, agents.by.row, as.vector
+...(... = c("pxcor", "pycor"), 
+              ... = "wolves")
 
 #############
 #  Patches  #
 #############
 
-# use NLGetPatches()
-# use NLSetPatches()
+...(... = c("pxcor", "pycor", ...),
+             ... = "patches with [...]")
+
+NLSetPatches(patch.var = ..., 
+             in.matrix = matrix(...))
+
+
 
